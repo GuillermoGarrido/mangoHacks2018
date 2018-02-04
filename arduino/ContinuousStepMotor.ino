96b041a50b163a5a69f8c5c00f083d64dd0b4b01
Guillermo Garrido <GuillermoGarrido@users.noreply.github.com>
2018-02-04 10:11:04 -0500

    Update ContinuousStepMotor.ino

--- arduino/ContinuousStepMotor.ino
+++ arduino/ContinuousStepMotor.ino
@@ -4,18 +4,32 @@ const int stepsPerRevolution = 200;  // change this to fit the number of steps p
 
 // initialize the stepper library on pins 3 through 6:
 Stepper myStepper(stepsPerRevolution, 3, 4, 5, 6);
+Stepper secondStepper(stepsPerRevolution, 8, 9, 10, 11);
 
 void setup() {
   // set the speed at 60 rpm:
   myStepper.setSpeed(60);
   // initialize the serial port:
   Serial.begin(9600);
+
+  secondStepper.setSpeed(60);
+  // initialize the serial port:
+  Serial.begin(9600);
 }
 
 void loop() {
   // step one revolution  in one direction:
+
+    
+  
   Serial.println("clockwise");
   myStepper.step(stepsPerRevolution);
+  Serial.println("clockwise");
+  secondStepper.step(stepsPerRevolution);
   delay(500);
 
 }
+  /*Serial.println("clockwise");
+  secondStepper.step(stepsPerRevolution);
+  delay(500); */
+ 
